36f2c635425777eb2d77cd7e512fbd36d1b21891
phatnguyen03022001 <117011854+phatnguyen03022001@users.noreply.github.com>
2026-08-18 23:57:52 +0700

    refactor: register customer booking boundary

--- server/src/modules/customer/customer.module.ts
+++ server/src/modules/customer/customer.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CustomerBookingService } from './customer-booking.service';
 import { CustomerController } from './customer.controller';
 import { CustomerPaymentService } from './customer-payment.service';
 import { CustomerService } from './customer.service';
 
 @Module({
   controllers: [CustomerController],
-  providers: [CustomerService, CustomerPaymentService],
-  exports: [CustomerService, CustomerPaymentService],
+  providers: [CustomerService, CustomerPaymentService, CustomerBookingService],
+  exports: [CustomerService, CustomerPaymentService, CustomerBookingService],
 })
 export class CustomerModule {}
