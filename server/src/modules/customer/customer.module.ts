185b110e8c5a759839248483b653613e2cb95245
phatnguyen03022001 <117011854+phatnguyen03022001@users.noreply.github.com>
2026-08-18 23:08:56 +0700

    security: wire hardened customer payment service

--- server/src/modules/customer/customer.module.ts
+++ server/src/modules/customer/customer.module.ts
@@ -1,24 +1,17 @@
-// src/modules/customer/customer.module.ts
 import { Module } from '@nestjs/common';
 import { CustomerController } from './customer.controller';
 import { CustomerService } from './customer.service';
-import { PrismaService } from '../../prisma/prisma.service';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SecureCustomerService } from './secure-customer.service';
 
 @Module({
-  imports: [
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '7d' },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
   controllers: [CustomerController],
-  providers: [CustomerService, PrismaService],
+  providers: [
+    SecureCustomerService,
+    {
+      provide: CustomerService,
+      useExisting: SecureCustomerService,
+    },
+  ],
   exports: [CustomerService],
 })
 export class CustomerModule {}
