926b34f2f99c4315744c78be8f5fdf15e224d169
phatnguyen03022001 <117011854+phatnguyen03022001@users.noreply.github.com>
2026-08-19 02:37:43 +0700

    register settlement accounting service

--- server/src/modules/admin/admin.module.ts
+++ server/src/modules/admin/admin.module.ts
@@ -5,6 +5,7 @@ import { AdminFinanceService } from './admin-finance.service';
 import { AdminPaymentReconciliationController } from './admin-payment-reconciliation.controller';
 import { AdminPromotionService } from './admin-promotion.service';
 import { AdminRefundService } from './admin-refund.service';
+import { AdminSettlementService } from './admin-settlement.service';
 import { AdminWalletReconciliationController } from './admin-wallet-reconciliation.controller';
 import { AdminService } from './admin.service';
 import { CustomerModule } from '../customer/customer.module';
@@ -30,12 +31,14 @@ import { PaymentModule } from '../payment/payment.module';
     AdminFinanceService,
     AdminPromotionService,
     AdminRefundService,
+    AdminSettlementService,
   ],
   exports: [
     AdminService,
     AdminFinanceService,
     AdminPromotionService,
     AdminRefundService,
+    AdminSettlementService,
   ],
 })
 export class AdminModule {}
