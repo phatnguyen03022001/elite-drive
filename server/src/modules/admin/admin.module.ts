ade0f83af0fc069bf7c7fe52da211aaaef09462c
phatnguyen03022001 <117011854+phatnguyen03022001@users.noreply.github.com>
2026-08-18 23:18:10 +0700

    refactor: split admin finance domain service

--- server/src/modules/admin/admin.module.ts
+++ server/src/modules/admin/admin.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AdminService } from './admin.service';
-import { SecureAdminService } from './secure-admin.service';
 import { AdminController } from './admin.controller';
+import { AdminFinanceService } from './admin-finance.service';
+import { AdminService } from './admin.service';
 import { CustomerModule } from '../customer/customer.module';
 
 @Module({
   imports: [CustomerModule],
   controllers: [AdminController],
-  providers: [
-    SecureAdminService,
-    {
-      provide: AdminService,
-      useExisting: SecureAdminService,
-    },
-  ],
-  exports: [AdminService],
+  providers: [AdminService, AdminFinanceService],
+  exports: [AdminService, AdminFinanceService],
 })
 export class AdminModule {}
