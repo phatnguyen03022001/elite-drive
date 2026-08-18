83edf3b58f3d3314f77d3cb105e920d4ef842233
phatnguyen03022001 <117011854+phatnguyen03022001@users.noreply.github.com>
2026-08-19 02:23:13 +0700

    refactor(customer): register contract service

--- server/src/modules/customer/customer.module.ts
+++ server/src/modules/customer/customer.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { CustomerBookingService } from './customer-booking.service';
 import { CustomerCancellationService } from './customer-cancellation.service';
+import { CustomerContractService } from './customer-contract.service';
 import { CustomerController } from './customer.controller';
 import { CustomerPaymentService } from './customer-payment.service';
 import { CustomerPromotionService } from './customer-promotion.service';
@@ -24,6 +25,7 @@ import { CustomerService } from './customer.service';
     CustomerPromotionService,
     CustomerCancellationService,
     CustomerReviewService,
+    CustomerContractService,
   ],
   exports: [
     CustomerService,
@@ -32,6 +34,7 @@ import { CustomerService } from './customer.service';
     CustomerPromotionService,
     CustomerCancellationService,
     CustomerReviewService,
+    CustomerContractService,
   ],
 })
 export class CustomerModule {}
