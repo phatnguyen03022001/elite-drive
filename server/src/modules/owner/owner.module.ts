457f4f56e644bbb0bd7c6d612f303b789b2f4336
phatnguyen03022001 <117011854+phatnguyen03022001@users.noreply.github.com>
2026-08-18 22:05:42 +0700

    fix(security): inject atomic withdraw service

--- server/src/modules/owner/owner.module.ts
+++ server/src/modules/owner/owner.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 
 import { OwnerService } from './owner.service';
+import { SecureOwnerService } from './secure-owner.service';
 import { OwnerController } from './owner.controller';
 
 @Module({
   controllers: [OwnerController],
-  providers: [OwnerService],
+  providers: [
+    SecureOwnerService,
+    {
+      provide: OwnerService,
+      useExisting: SecureOwnerService,
+    },
+  ],
 })
 export class OwnerModule {}
