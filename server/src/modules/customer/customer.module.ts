89138d9b4cdeca65fbfed95ab3a9059268f7a181
phatnguyen03022001 <117011854+phatnguyen03022001@users.noreply.github.com>
2026-08-19 00:22:39 +0700

    security: cap buffered customer uploads

--- server/src/modules/customer/customer.module.ts
+++ server/src/modules/customer/customer.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { CustomerBookingService } from './customer-booking.service';
 import { CustomerController } from './customer.controller';
 import { CustomerPaymentService } from './customer-payment.service';
 import { CustomerService } from './customer.service';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      limits: {
+        fileSize: 5 * 1024 * 1024,
+      },
+    }),
+  ],
   controllers: [CustomerController],
   providers: [CustomerService, CustomerPaymentService, CustomerBookingService],
   exports: [CustomerService, CustomerPaymentService, CustomerBookingService],
